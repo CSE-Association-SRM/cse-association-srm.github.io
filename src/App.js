b186f4873b02562591e2a6f0de94e88404a7a5b4
Shubham Deodhar <deodhar2000@gmail.com>
2020-06-08 15:43:22 +0530

    'Day1'

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import HomeNav from './Components/HomeNavComponenet';
+function App() {
+  return (
+   <HomeNav/>
+  );
+}
+
+export default App;
---
+++ src/Components/CarouselComponent.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import '../App.css';
+
+const Carousel=()=>{
+    return(
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="/assets/trial4.jpg" class="d-block w-100" alt="..."/>
+    </div>
+    <div class="carousel-item">
+      <img src="/assets/trial_2.0.jpg" class="d-block w-100" alt="..."/>
+    </div>
+    <div class="carousel-item">
+      <img src="/assets/trial_3.0.jpg" class="d-block w-100" alt="..."/>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+       
+    );
+}
+
+export default Carousel;
